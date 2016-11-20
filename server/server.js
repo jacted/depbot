e981e3381afb14bccd9200e6c05a1201c4719db6
jacted <jacted@users.noreply.github.com>
2016-11-20 19:42:53 +0100

    Refactor api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express')
 const server = express()
 const bodyParser = require('body-parser')
 const xhub = require('express-x-hub')
-const DeployerJS = require('deployer-js')
 const config = require('../config')
 
 // Github secret + bodyparser
@@ -17,45 +16,6 @@ server.get('/', (req, res) => {
 })
 
 // API
-server.post('/git/webhook', (req, res) => {
-  if (!req.isXHub || !req.isXHubValid()) {
-    res.status(401).end('Wrong signature.')
-  } else {
-
-    // Get project
-    let project = config.projects[req.body.repository.name]
-    if (typeof project === 'undefined') {
-      res.status(500).end('Project does not exist.')
-      return false
-    }
-
-    if (req.body.ref === 'refs/heads/' + project.git.branch) {
-    
-      try {
-
-        let deployer = new DeployerJS({
-          ftp: project.ftp,
-          git: project.git
-        })
-
-        deployer.deployCommitedFiles(req.body.commits).then((res) => {
-          console.log(res)
-        }, (err) => {
-          console.log(err)
-        })
-
-        res.end('OK - Deploying')
-
-      } catch (e) {
-        console.log(e)
-        res.status(500).end('Error')
-      }
-      
-    } else {
-      res.end('OK - Not deploying')
-    }
-
-  }
-})
+require('./api')(server)
 
 server.listen(config.port, function () {})
